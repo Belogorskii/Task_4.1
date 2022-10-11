6ae7a1b42d61f70539418012eca2bba7a2ed7cd4
Kirill Miller <belogorski2000@gmail.com>
2022-10-11 17:18:36 +0300

    Добавьте файлы проекта.

---
+++ Task_4.1/Main.cs
@@ -0,0 +1,50 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4._1
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            string wallInfo = string.Empty;
+            var wallss = new FilteredElementCollector(doc)
+                .OfCategory(BuiltInCategory.OST_Walls)
+                .Cast<Wall>()
+                .ToList();
+            string excelPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "rooms.xlsx");
+            using (FileStream stream = new FileStream(excelPath, FileMode.Create, FileAccess.Write))
+            {
+                IWorkbook workbook = new XSSFWorkbook();
+                ISheet sheet = workbook.CreateSheet("List 1");
+
+                int rowIndex = 0;
+                foreach(Wall wall in wallss)
+                {
+                    sheet.SetCellValue(rowIndex, columnIndex: 0, wall.Name);
+                    sheet.SetCellValue(rowIndex, columnIndex: 1, wall.Volume);
+                    rowIndex++;
+                }
+                workbook.Write(stream);
+                workbook.Close();
+            }
+            System.Diagnostics.Process.Start(excelPath);
+            return Result.Succeeded;
+        } 
+    }
+}
